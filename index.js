92d744a7371a195b78830e1bc2c4db04576adcd2
KSLND <sasadragin@tehnickacacak.edu.rs>
2025-05-26 13:19:22 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let links = document.querySelectorAll(".nav-link");
+
+    links.forEach(link => {
+        link.addEventListener("click", function (event) {
+            event.preventDefault(); // Sprečava podrazumevano ponašanje
+            
+            // Uklanja "active" klasu sa svih linkova
+            links.forEach(link => link.classList.remove("active"));
+
+            // Dodaje "active" klasu kliknutom linku
+            this.classList.add("active");
+
+            // Pomeri stranicu ka sekciji
+            let targetId = this.getAttribute("href"); 
+            let targetSection = document.querySelector(targetId);
+            
+            if (targetSection) {
+                targetSection.scrollIntoView({ behavior: "smooth" });
+            }
+        });
+    });
+});
